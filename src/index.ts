4507abc5fd9cee6fa7a94cf7a70eff2526a6c5f7
Disha2597 <dvaswani2597@gmail.com>
2023-10-19 12:23:16 -0400

    added index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "./styles/index.css";
+
+export const greet = () => {
+    return "Hello World from Code with Ahsan!";
+  }
+  
+  const main = () => {
+    console.log(greet());
+  };
+  
+  main();
